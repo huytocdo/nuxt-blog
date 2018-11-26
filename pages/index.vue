5f1835973efacfcca75b4d00e1cceee69b437fe7
huytocdo <ndhuyspkt@gmail.com>
2018-11-26 15:29:53 +0700

    test

--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,8 @@
 <template>
   <section class="container">
-    <div>
-      <logo/>
-      <h1 class="title">
-        my-blog
-      </h1>
-      <h2 class="subtitle">
-        My blog build with nuxt.js
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
-    </div>
+    <h1 class="title">
+      my-blog
+    </h1>
   </section>
 </template>
 
